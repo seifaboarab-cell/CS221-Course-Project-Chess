53080dc5de52d76fd42325b28eae493a6793764e
mohanned <mohannad.ahmed666@gmail.com>
2025-12-01 18:24:49 +0200

    Created the main loop for the game

--- src/main.c
+++ src/main.c
@@ -6,6 +6,21 @@
 #include "queen.h"
 #include "rook.h"
 
+#include <ctype.h>
+#include <stdio.h>
+
 int main(){
+    char *players[] = {"White", "Black"};
+    for (int i = 0;;){
+        int turn = i / 2 + 1, player_number = i % 2;
+        char input[10];
+        display();
+        printf("%d-%s: ", turn, players[player_number]);
+        scanf("%s", input);
+        int x1 = toupper(input[0]) - 'A', y1 = input[1] - '1', x2 = toupper(input[2]) - 'A', y2 = input[3] - '1';
+        //if (!move(x1, y1, x2, y2, player_number))
+            //continue;
+        i++;
+    }
     return 0;
 }
